787f43e473cdbf9fece03e25a29c4f54082517e5
meowme0ww <robertabcdefg4@gmail.com>
2025-11-16 20:02:51 +0800

    Update rank.js

--- api/rank.js
+++ api/rank.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const players = [
     { name: "PlayerOne", tier: "Diamond", rank: "III", lp: 88 },
     { name: "NightWolf", tier: "Master", rank: "I", lp: 165 },
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     { name: "Storm", tier: "Emerald", rank: "II", lp: 75 }
   ];
 
-  // Sort highest LP first
+  // Sort by highest LP
   players.sort((a, b) => b.lp - a.lp);
 
   const formatted = players
